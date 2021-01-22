504f32db61976bbff00d7ec45243dddfd843918b
GabrielDEV <gabrielfernella07@hotmail.com>
2021-01-21 23:32:07 -0300

    CRUDCompleted in NodeJS
    Completed CRUD NodeJS
    ADDs
    findById
    Update
    Delete...

--- nodejs/customers/src/controllers/customer-controller.js
+++ nodejs/customers/src/controllers/customer-controller.js
@@ -1,12 +1,82 @@
-const neDB = require('../configurations/database')
-const api = {}
+const neDB = require('../configurations/database');
+const api = {};
 
 api.findAll = (request, response) => {
+  neDB
+    .find({})
+    .sort({ name: 1 })
+    .exec((exception, customers) => {
+      if (exception) {
+        console.log('Opa, deu ruim na tentativa de listar todos os customers', exception);
+      }
 
-}
+      response.json(customers);
+    });
+};
 
-api.getById = (request, response) => {
+api.save = (request, response) => {
+  const canonical = request.body;
 
-}
+  neDB.insert(canonical, (exception, customers) => {
+    if (exception) {
+      console.log('Opa, deu ruim na tentativa de inserir o customers', exception);
+    }
 
-module.exports = api
+    response.status(201).json(customers);
+  });
+};
+
+api.findOne = (request, response) => {
+  const req = request.params.id;
+  console.log(req);
+
+  neDB.findOne({ name: req }).exec((exception, customers) => {
+    if (exception || customers === null) {
+      const setence = 'Customer not found';
+      console.log(setence, exception);
+      response.status(400);
+      response.json({ mensagem: setence });
+    }
+    response.json(customers);
+  });
+};
+
+api.update = (request, response) => {
+  const req = request.params.id;
+  const { name, age, description } = request.body;
+
+  console.log(name, age, description);
+
+  neDB.update(
+    { _id: req },
+    { name: name, age: age, description: description },
+    (exception, customers) => {
+      if (exception || customers === 0) {
+        const setence = 'Custumer not found';
+        console.log(setence, exception);
+        response.status(400).json({ mensagem: setence });
+      }
+      response.json(customers);
+    }
+  );
+};
+
+api.delete = (request, response) => {
+  const req = request.params.id;
+
+  console.log(req);
+
+  neDB.remove({ _id: req }, {}, (exception, customers) => {
+    if (exception || customers === 0) {
+      const setence = 'Customer not found';
+      console.log(setence, exception);
+      return response.status(400).json({ mensagem: setence });
+    } else {
+      return response.status(200).json(customers);
+    }
+  });
+};
+
+module.exports = api;
+
+//https://github.com/louischatriot/nedb
--- nodejs/customers/src/routes/customer-route.js
+++ nodejs/customers/src/routes/customer-route.js
@@ -1,8 +1,7 @@
-const api = require('../controllers/customer-controller')
+const api = require('../controllers/customer-controller');
 
 module.exports = (app) => {
-    app.route('/customers')
-        .get(api.findAll)
-        .post(api.post)
+  app.route('/customers').get(api.findAll).post(api.save);
 
-}
+  app.route('/customers/:id').get(api.findOne).put(api.update).delete(api.delete);
+};
